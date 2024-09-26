55f6132644fe647f60218d8eb2ba0c367e938c53
lucianegural <126890538+lucianegural@users.noreply.github.com>
2024-09-26 08:18:52 -0300

    concluido

--- script.js
+++ script.js
@@ -34,24 +34,20 @@ const perguntas = [
                 texto: "Um dos pincipais desafios é a falta de recursos financeiros e infraestruturas adequada para executar projetos de conservação em regiões mais pobres.",
             },
             {
-                texto: "Outro desafio e a .",
+                texto: "Outro desafio e a resistência cultural e econômica de algumas comunidades que dependem de atividades que causam danos ambientais.",
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "De que forma as mudanças climáticas afetam a biodiversidade, e quais efeitos isso podem ter a longo prazo no equilíbrio dos ecossistemas?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "As mudanças climáticas levam a alteração de habitantes naturais, resultando na extinção de espécies que não consequem se adptar rapidamente, o que afeta negativamente a biodiversidade.",
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "A perda da biodiversidade pode comprometer os serviços ecossistêmicos essenciais, como a polinização de plantas e purificação da água.",
             }
         ]
-    },
-    {
         enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
         alternativas: [
             {
@@ -99,7 +95,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "O meio ambiene é um tema complexo que envolve uma variedade de fatores interligados, desde as ações individuas até as políticas governamentais. A conscientização sobre a impotância da conservação é vital, uma vez que as atividades humanas têm um impacto significativo sobre a natureza. Discutir as medidas que podem ser tomadas, os desafios enfrentados e os efeitos das mudanças climáticas ajudam a compreender a urgência desse assunto.A educação e a participação ativa da sociedade são aspectos fundamentais para garantir um futuro sustentável."
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
